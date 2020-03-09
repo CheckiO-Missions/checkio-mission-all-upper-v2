632aaff38f4d9a55c0a162d94d9dc7d025661086
Alexander Lyabah <a.lyabah@checkio.org>
2020-03-09 12:52:43 +0200

    first commit

---
+++ autofill.sh
@@ -0,0 +1,1 @@
+checkio --domain py autofillrepo --py-function is_all_upper --js-function isAllUpper --desc-tests 4 .
---
+++ verification/tests.py
@@ -0,0 +1,53 @@
+"""
+TESTS is a dict with all of your tests.
+Keys for this will be the categories' names.
+Each test is a dict with
+    "input" -- input data for a user function
+    "answer" -- your right answer
+    "explanation" -- not necessarily a key, it's used for an additional info in animation.
+"""
+
+
+TESTS = {
+    "Basics": [
+        {
+            "input": ['ALL UPPER'],
+            "answer": True,
+        },
+        {
+            "input": ['all lower'],
+            "answer": False,
+        },
+        {
+            "input": ['mixed UPPER and lower'],
+            "answer": False,
+        },
+        {
+            "input": [''],
+            "answer": False,
+        }
+    ],
+    "Extra": [
+        {
+            "input": ['Hi'],
+            "answer": False,
+        },
+        {
+            "input": ['     '],
+            "answer": False,
+        },
+        {
+            "input": ['123'],
+            "answer": False,
+        },
+        {
+            "input": ['DIGITS123'],
+            "answer": True,
+        },
+
+        {
+            "input": ['WORLD'],
+            "answer": True,
+        }
+    ]
+}
